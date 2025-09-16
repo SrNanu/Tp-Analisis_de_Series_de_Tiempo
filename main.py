fc7bc8c27bce4e983db3cfee114cc3477d42e9e3
SrNanu <107449931+SrNanu@users.noreply.github.com>
2025-09-16 16:34:59 -0300

    Cambio de main para que se entienda mas

--- main.py
+++ main.py
@@ -13,27 +13,27 @@ def main():
     df, train, test = data_loader.cargar_y_preparar_datos(url)
     
     # Datos iniciales
-    df['y'].plot(title='Número de Pasajeros de Aerolínea Mensuales (1949-1960) en millones', figsize=(15,7))
+    df['y'].plot(title='Número de Pasajeros de Aerolínea Mensuales (1949-1960) en miles', figsize=(15,7))
     plt.show() 
 
     # Modelos
-    naive_preds, rmse_naive = model_naive.run(train, test)
-    prophet_preds, rmse_prophet = model_prophet.run(train, test)
-    lstm_preds, rmse_lstm = model_lstm.run(train, test)
+    predicciones_naive, rmse_naive = model_naive.run(train, test)
+    predicciones_prophet, rmse_prophet = model_prophet.run(train, test)
+    predicciones_lstm, rmse_lstm = model_lstm.run(train, test)
     
     # Comparación final
     print("\n--- Mostrando Comparación Final de Modelos ---")
-    test_predictions = test.copy()
-    test_predictions['Naive'] = naive_preds
-    test_predictions['Prophet'] = prophet_preds
-    test_predictions['LSTM'] = lstm_preds
+    predicciones = test.copy()
+    predicciones['Naive'] = predicciones_naive
+    predicciones['Prophet'] = predicciones_prophet
+    predicciones['LSTM'] = predicciones_lstm
     
     plt.figure(figsize=(14, 6))
     plt.plot(train['y'], label='Datos de Entrenamiento')
-    plt.plot(test_predictions['y'], label='Datos Reales (Prueba)', color='black', lw=2)
-    plt.plot(test_predictions['Naive'], label='Predicción Naive', linestyle='--')
-    plt.plot(test_predictions['Prophet'], label='Predicción Prophet', linestyle='--')
-    plt.plot(test_predictions['LSTM'], label='Predicción LSTM', linestyle='--')
+    plt.plot(predicciones['y'], label='Datos Reales (Prueba)', color='black', lw=2)
+    plt.plot(predicciones['Naive'], label='Predicción Naive', linestyle='--')
+    plt.plot(predicciones['Prophet'], label='Predicción Prophet', linestyle='--')
+    plt.plot(predicciones['LSTM'], label='Predicción LSTM', linestyle='--')
     plt.title('Comparación de Modelos de Pronóstico', fontsize=20)
     plt.xlabel('Fecha', fontsize=14)
     plt.ylabel('Número de Pasajeros', fontsize=14)
